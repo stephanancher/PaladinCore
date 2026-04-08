ad0ff811d0bdbd818f262f7d9fd29efe95974715
Stephan Ancher <stephan@example.com>
2026-04-09 00:10:33 +0200

    Fixed menu transparency using solid background texture

--- PaladinCore.lua
+++ PaladinCore.lua
@@ -1,6 +1,6 @@
 PCA_Config = PCA_Config or {}
 
-local PCA_VERSION = "1.4.1"
+local PCA_VERSION = "1.4.2"
 
 local defaultOpener        = "Holy Strike"
 local defaultOpenerPrebuff = "Seal of Righteousness"
