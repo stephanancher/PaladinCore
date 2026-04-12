0f4f4347d13cda6c029786a1ac08f3222793e79f
Stephan Ancher <stephan@example.com>
2026-04-12 17:18:31 +0200

    Version 2.0.0 - Major update

--- PaladinCore.lua
+++ PaladinCore.lua
@@ -1,6 +1,6 @@
 PCA_Config = PCA_Config or {}
 
-local PCA_VERSION = "1.9.1"
+local PCA_VERSION = "2.0.0"
 
 -- Use tables to avoid "too many upvalues" limit (limit=32 in Lua 5.0/Vanilla)
 local PCA_Refs  = {}
